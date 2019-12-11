29bba7411f1ae42579636a838e271cb5eb9765c0
Phpner <phpner@gmail.com>
2019-12-11 13:54:17 +0300

    commit 2

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,10 +7,32 @@ $(document).ready(function ($) {
             centerMode: true,
             centerPadding: '60px',
             slidesToShow: 3,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1,
+                        infinite: true,
+                        adaptiveHeight: false,
+                        dots: true
+                    }
+                }
+            ]
     });
 
     $( "#accordion" ).accordion({
         autoHeight: true,
         navigation: true
     });
+
+    setTimeout(function(){
+        Array.prototype.forEach.call(
+            document.querySelectorAll('img[data-src]'), function(img) {
+                img.setAttribute('src', img.getAttribute('data-src'));
+                img.onload = function() {
+                    img.removeAttribute('data-src');
+                };
+            }
+        );
+    },2000);
 });
